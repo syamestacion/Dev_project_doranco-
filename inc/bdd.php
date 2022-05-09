1591dd644e4b6a5027d9471f923f0405f7b83b5f
karim <karim.ammi@hotmail.com>
2022-05-09 16:36:25 +0200

    function et bdd

---
+++ inc/bdd.php
@@ -0,0 +1,20 @@
+<?php
+// Conexion à la base de données
+$pdo = new PDO('mysql:host=localhost;dbname=womart', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+// var_dump($pdo);
+
+
+// Creation de la session. Permet de stocker des infos sur les internautes
+session_start();
+
+// Definition de constantes
+// POUR WAMP
+//define('RACINE_SITE', $_SERVER['DOCUMENT_ROOT'].'/');
+//define('URL', '/');
+
+// Pour XAMP ET MAMP
+define('RACINE_SITE', $_SERVER['DOCUMENT_ROOT'].'/Dev_project_doranco-/');
+define('URL', 'http://localhost:8888/Dev_project_doranco-/');
+
+$content = '';
+?>
